173c244f5cc8851de0da18b6a53c7be5ac586cb9
Felipe Mendoza <felipe.domingo.mendoza@gmail.com>
2023-02-18 00:17:00 -0300

    implementacion de Formik

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -3,6 +3,7 @@ import { Link, Navigate } from "react-router-dom"
 import { useCartContext } from "../../context/CartContext"
 import { db } from "../../firebase/config"
 import { collection, addDoc, doc } from "firebase/firestore"
+import { Formik, Form, Field, ErrorMessage } from "formik"
 
 const Checkout = () => {
     const { cart, totalCart, emptycart } = useCartContext()
@@ -23,21 +24,31 @@ const Checkout = () => {
 
     }
 
+
+    if (ordenId) {
+        return (
+            <div className="container">
+                <div className="row my-4 justify-content-center">
+                    <div className="col-md-12 align-middle text-center m-3">
+                        <h2>Gracias por tu compra!</h2>
+                      
+                        {ordenId && <div className="alert alert-success" role="alert">Orden generada: <b>{ordenId}</b></div>}
+
+
+                    </div>
+                    <Link to="/" className="col-md-2 btn btn-primary align-self-center text-center m-3 mb-2">Ir a inicio</Link>
+                </div>
+            </div>
+        )
+    }
+
+    if (cart.length === 0) {
+        return <Navigate to="/" />
+    }
     const handleSubmit = (e) => {
-        e.preventDefault()
+
         //Validacion
-        if (values.nombre.length < 2) {
-            alert("Nombre Invalido")
-            return
-        }
-        if (values.direccion.length < 2) {
-            alert("Direccion Invalida")
-            return
-        }
-        if (values.email.length < 2) {
-            alert("email Invalido")
-            return
-        }
+
         const orden = {
             cliente: values,
             items: cart,
@@ -54,26 +65,6 @@ const Checkout = () => {
             })
             .catch((error) => console.log(error))
     }
-    if (ordenId) {
-        return (
-            <div className="container">
-                <div className="row my-4 justify-content-center">
-                    <div className="col-md-12 align-middle text-center m-3">
-                        <h2>Gracias por tu compra!</h2>
-                        {ordenId && <div className="alert alert-success" role="alert">Orden generada: <b>{ordenId}</b></div>}
-
-
-                    </div>
-                    <Link to="/" className="col-md-2 btn btn-primary align-self-center text-center m-3 mb-2">Ir a inicio</Link>
-                </div>
-            </div>
-        )
-    }
-
-    if (cart.length === 0) {
-        return <Navigate to="/" />
-    }
-
     return (
         <div className="container checkout">
             <div className="row my-5">
@@ -82,71 +73,151 @@ const Checkout = () => {
                     <h2>Termina mi compra</h2>
                     <hr />
 
-                    <form onSubmit={handleSubmit}>
-                        <div className="mb-3">
-                            <label className="form-label">Nombre</label>
-                            <input
-                                onChange={handleInputChange}
-                                type="text"
-                                className="form-control"
-                                id="name"
-                                name="nombre"
-                                placeholder="Ingrese su nombre"
-                                value={values.nombre}
-                            />
-                        </div>
-                        <div className="mb-3">
-                            <label className="form-label">Apellido</label>
-                            <input
-                                onChange={handleInputChange}
-                                type="text"
-                                className="form-control"
-                                id="apellido"
-                                name="apellidp"
-                                placeholder="Ingrese su Apellido"
-                                value={values.apellido}
-                            />
-                        </div>
-                        <div className="mb-3">
-                            <label className="form-label">Dirección</label>
-                            <input
-                                onChange={handleInputChange}
-                                type="text"
-                                className="form-control"
-                                id="direccion"
-                                name="direccion"
-                                placeholder="Ingrese su dirección"
-                                value={values.direccion}
-                            />
-                        </div>
-                        <div className="mb-3">
-                            <label className="form-label">Teléfono</label>
-                            <input
-                                onChange={handleInputChange}
-                                type="text"
-                                className="form-control"
-                                id="telefono"
-                                name="telefono"
-                                placeholder="Ingrese su telefono"
-                                value={values.telefono}
-                            />
-                        </div>
-                        <div className="mb-3">
-                            <label className="form-label">E-mail</label>
-                            <input
-                                onChange={handleInputChange}
-                                type="text"
-                                className="form-control"
-                                id="email"
-                                name="email"
-                                placeholder="Ingrese su email"
-                                value={values.email}
-                            />
-                        </div>
-                        <button className="btn btn-primary">Enviar</button>
 
 
-                    </form>
+                    <Formik
+                        initialValues={{
+                            nombre: '',
+                            apellido: '',
+                            telefono:'',
+                            direccion:'',
+                            correo: '',
+
+                        }}
+                        validate={(valores) => {
+                            let errores = {};
+                            //Validacion nombre
+                            if (!valores.nombre) {
+                                errores.nombre = 'Por favor ingresa un nombre'
+                            } else if (!/^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(valores.nombre)) {
+                                errores.nombre = 'El nombre solo puede contener letras y espacios'
+                            }
+
+                            //Validacion apellido
+                            if (!valores.apellido) {
+                                errores.apellido = 'Por favor ingresa un apellido'
+                            } else if (!/^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(valores.apellido)) {
+                                errores.nombre = 'El apellido solo puede contener letras y espacios'
+                            }
+                            //Validacion telefono
+
+                            //Validacion correo
+                            if (!valores.email) {
+                                errores.email = 'Por favor ingresa un nombre'
+                            } else if (!/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(valores.email)) {
+                                errores.email = 'El email solo puede contener letras, numeros, puntos, guiones y guion bajo '
+                            }
+
+
+
+                            return errores;
+
+                        }
+                        }
+                        onSubmit={handleSubmit}
+
+                    >
+                        {
+                            ({ values, errors, touched, handleSubmit, handleChange, handleBlur }) => (
+
+
+                                <Form>
+                                    <div className="mb-3">
+                                        <label htmlFor="nombre" className="form-label">Nombre</label>
+                                        <Field
+
+                                            type="text"
+                                            className="form-control"
+                                            id="name"
+                                            name="nombre"
+                                            placeholder="Ingrese su nombre"
+
+                                        />
+                                        <ErrorMessage name="nombre" component={() => (
+
+                                            <div className="error"> {errors.nombre} </div>
+                                        )} />
+
+                                    </div>
+
+                                    <div className="mb-3">
+                                        <label htmlFor="apellido" className="form-label">Apellido</label>
+                                        <Field
+                                            type="text"
+                                            className="form-control"
+                                            id="apellido"
+                                            name="apellido"
+                                            placeholder="Ingrese su Apellido"
+                                            value={values.apellido}
+                                            onChange={handleChange}
+                                            onBlur={handleBlur}
+                                        />
+
+                                        {errors.apellido && <div className="error">
+                                            {errors.apellido}
+                                        </div>}
+                                    </div>
+                                    <div className="mb-3">
+                                        <label htmlFor="direccion" className="form-label">Dirección</label>
+                                        <Field
+
+                                            type="text"
+                                            className="form-control"
+                                            id="direccion"
+                                            name="direccion"
+                                            placeholder="Ingrese su dirección"
+                                            value={values.direccion}
+                                            onChange={handleChange}
+                                            onBlur={handleBlur}
+                                        />
+                                        {errors.direccion && <div className="error">
+                                            {errors.direccion}
+                                        </div>}
+                                    </div>
+                                    <div className="mb-3">
+                                        <label htmlFor="telefono" className="form-label">Teléfono</label>
+                                        <Field
+
+                                            type="text"
+                                            className="form-control"
+                                            id="telefono"
+                                            name="telefono"
+                                            placeholder="Ingrese su telefono"
+                                            value={values.telefono}
+                                            onChange={handleChange}
+                                            onBlur={handleBlur}
+                                        />
+                                        {errors.telefono && <div className="error">
+                                            {errors.telefono}
+                                        </div>}
+                                    </div>
+                                    <div className="mb-3">
+                                        <label htmlFor="email" className="form-label">E-mail</label>
+                                        <Field
+
+                                            type="email"
+                                            className="form-control"
+                                            id="email"
+                                            name="email"
+                                            placeholder="Ingrese su email"
+                                            value={values.email}
+                                            onChange={handleChange}
+                                            onBlur={handleBlur}
+                                        />
+                                        {errors.email && <div className="error">
+                                            {errors.email}
+                                        </div>}
+                                    </div>
+                                    <button className="btn btn-primary" type="submit">Enviar</button>
+
+
+                                </Form>
+
+
+                            )
+                        }
+
+                    </Formik>
                 </div>
                 <div className="col-md-6">
                     <table className="table">
