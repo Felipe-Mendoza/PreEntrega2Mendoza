33e9ca8370e10bbc03aeb10d89bab6a5612c4788
Felipe Mendoza <felipe.domingo.mendoza@gmail.com>
2023-02-19 19:42:46 -0300

    Mejora en la validación con Formik

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -2,28 +2,73 @@ import { useState } from "react"
 import { Link, Navigate } from "react-router-dom"
 import { useCartContext } from "../../context/CartContext"
 import { db } from "../../firebase/config"
-import { collection, addDoc, doc } from "firebase/firestore"
-import { Formik, Form, Field, ErrorMessage } from "formik"
+import { collection, addDoc} from "firebase/firestore"
+import { useFormik } from "formik"
+import './Checkout.css';
 
 const Checkout = () => {
     const { cart, totalCart, emptycart } = useCartContext()
 
     const [ordenId, setOrdenId] = useState(null)
 
-    const [values, setValues] = useState({nombre: '',
-    apellido: '',
-    telefono:'',
-    direccion:'',
-    email: ''})
-    
+    const validate = values => {
 
-    const handleInputChange = (e) => {
-        setValues({
-            ...values,
-            [e.target.name]: e.target.value
-        })
+        const errors = {};
+        if (!values.nombre) {
+            errors.nombre = 'Por favor ingresa un nombre'
+        } else if (!/^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(values.nombre)) {
+            errors.nombre = 'El nombre solo puede contener letras y espacios'
+        } 
+        
+        if (!values.apellidp) {
+            errors.apellidp = 'Por favor ingresa un apellido'
+        } else if (!/^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(values.apellidp)) {
+            errors.apellidp = 'El apellido solo puede contener letras y espacios'
+        } 
+        
+        if (!values.direccion) {
+            errors.direccion = 'Por favor ingresa una dirección'
+        }
 
+        if (!values.telefono) {
+            errors.telefono = 'Por favor ingresa un numero telefonico'
+        } else if (!/^[0-9]+$/.test(values.telefono)) {
+            errors.telefono = 'El número telefonico solo puede contener números'
+        }
+
+        //Validacion correo
+        if (!values.email) {
+            errors.email = 'Por favor ingresa un email'
+        } else if (!/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(values.email)) {
+            errors.email = 'El formato correcto será usuario@email.com'
+        }
+
+
+        return errors;
     }
+
+    const formik = useFormik({
+        initialValues: { nombre: '', apellidp: '', direccion: '', telefono: '', email: '' },
+        validate,
+        onSubmit: values => {
+            console.log(values)
+            const orden = {
+                cliente: values,
+                items: cart,
+                total: totalCart()
+            }
+
+
+            const ordenesRef = collection(db, "Ordenes")
+            addDoc(ordenesRef, orden)
+                .then((doc) => {
+                    setOrdenId(doc.id)
+                    emptycart()
+
+                })
+                .catch((error) => console.log(error))
+        }
+    })
 
 
     if (ordenId) {
@@ -32,10 +77,7 @@ const Checkout = () => {
                 <div className="row my-4 justify-content-center">
                     <div className="col-md-12 align-middle text-center m-3">
                         <h2>Gracias por tu compra!</h2>
-                  
-                        {ordenId && <div className="alert alert-success" role="alert">Número de orden generada: <b>{ordenId}</b></div>}
-         
-
+                        {ordenId && <div className="alert alert-success" role="alert">Orden generada: <b>{ordenId}</b></div>}
 
 
                     </div>
@@ -48,26 +90,7 @@ const Checkout = () => {
     if (cart.length === 0) {
         return <Navigate to="/" />
     }
-    const handleSubmit = (e) => {
 
-        //Validacion
-
-        const orden = {
-            cliente: values,
-            items: cart,
-            total: totalCart()
-        }
-
-
-        const ordenesRef = collection(db, "Ordenes")
-        addDoc(ordenesRef, orden)
-            .then((doc) => {
-                setOrdenId(doc.id)
-                emptycart()
-
-            })
-            .catch((error) => console.log(error))
-    }
     return (
         <div className="container checkout">
             <div className="row my-5">
@@ -76,168 +99,97 @@ const Checkout = () => {
                     <h2>Termina mi compra</h2>
                     <hr />
 
-
-
-                    <Formik
-                        initialValues={{
-                            nombre: '',
-                            apellido: '',
-                            telefono:'',
-                            direccion:'',
-                            email: '',
-
-                        }}
-                        validate={(valores) => {
-                            let errores = {};
-                            //Validacion nombre
-                            if (!valores.nombre) {
-                                errores.nombre = 'Por favor ingresa un nombre'
-                            } else if (!/^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(valores.nombre)) {
-                                errores.nombre = 'El nombre solo puede contener letras y espacios'
+                    <form onSubmit={formik.handleSubmit}>
+                        <div className="mb-3">
+                            <label className="form-label">Nombre</label>
+                            <input
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
+                                type="text"
+                                className="form-control"
+                                id="name"
+                                name="nombre"
+                                placeholder="Ingrese su nombre"
+                                value={formik.values.nombre}
+                            />
+                          
                                 
-                            }
+                                 {formik.touched.nombre && formik.errors.nombre ?(
+                                     <div className="errors"> {formik.errors.nombre} </div>
+                                 ):null} 
                           
 
-                            //Validacion apellido
-                            if (!valores.apellido) {
-                                errores.apellido = 'Por favor ingresa un apellido'
-                            } else if (!/^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(valores.apellido)) {
-                                errores.apellido = 'El apellido solo puede contener letras y espacios'
-                            }
-                              //Validacion Direccion
-                              if (!valores.direccion) {
-                                errores.direccion = 'Por favor ingresa una dirección'
-                            } 
-                            //Validacion telefono
-                            if (!valores.telefono) {
-                                errores.telefono = 'Por favor ingresa un numero telefonico'
-                            } else if (!/^[0-9]+$/.test(valores.telefono)) {
-                                errores.telefono = 'El número telefonico solo puede contener números'
-                            }
+                                 
 
-                            //Validacion correo
-                            if (!valores.email) {
-                                errores.email = 'Por favor ingresa un email'
-                            } else if (!/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(valores.email)) {
-                                errores.email = 'El email solo puede contener letras, numeros, puntos, guiones y guion bajo '
-                            }
-
-
-
-                            return errores;
-
-                        }
-                        }
-                        onSubmit={handleSubmit}
-
-                    >
-                        {
-                            ({ values, errors, touched, handleSubmit, handleChange, handleBlur }) => (
-
-
-                                <Form>
-                                    {console.log(values)}
-                                    <div className="mb-3">
-                                        <label htmlFor="nombre" className="form-label">Nombre</label>
-                                        <Field
-
-                                            type="text"
-                                            className="form-control"
-                                            id="name"
-                                            name="nombre"                                            
-                                            placeholder="Ingrese su nombre"
-                                            value={values.nombre}
-                                         
-                                           
-
-                                        />
-                                        <ErrorMessage name="nombre" component={() => (
-
-                                            <div className="error"> {errors.nombre} </div>
-                                        )} />
-
-
-
-                                    </div>
-
-                                    <div className="mb-3">
-                                        <label htmlFor="apellido" className="form-label">Apellido</label>
-                                        <Field
-                                            type="text"
-                                            className="form-control"
-                                            id="apellido"
-                                            name="apellido"
-                                            placeholder="Ingrese su Apellido"
-                                            value={values.apellido}
-                                            onChange={handleChange}
-                                            onBlur={handleBlur}
-                                        />
-
-                                        {errors.apellido && <div className="error">
-                                            {errors.apellido}
-                                        </div>}
-                                    </div>
-                                    <div className="mb-3">
-                                        <label htmlFor="direccion" className="form-label">Dirección</label>
-                                        <Field
-
-                                            type="text"
-                                            className="form-control"
-                                            id="direccion"
-                                            name="direccion"
-                                            placeholder="Ingrese su dirección"
-                                            value={values.direccion}
-                                            onChange={handleChange}
-                                            onBlur={handleBlur}
-                                        />
-                                        {errors.direccion && <div className="error">
-                                            {errors.direccion}
-                                        </div>}
-                                    </div>
-                                    <div className="mb-3">
-                                        <label htmlFor="telefono" className="form-label">Teléfono</label>
-                                        <Field
-
-                                            type="text"
-                                            className="form-control"
-                                            id="telefono"
-                                            name="telefono"
-                                            placeholder="Ingrese su telefono"
-                                            value={values.telefono}
-                                            onChange={handleChange}
-                                            onBlur={handleBlur}
-                                        />
-                                        {errors.telefono && <div className="error">
-                                            {errors.telefono}
-                                        </div>}
-                                    </div>
-                                    <div className="mb-3">
-                                        <label htmlFor="email" className="form-label">E-mail</label>
-                                        <Field
-
-                                            type="email"
-                                            className="form-control"
-                                            id="email"
-                                            name="email"
-                                            placeholder="Ingrese su email"
-                                            value={values.email}
-                                            onChange={handleChange}
-                                            onBlur={handleBlur}
-                                        />
-                                        {errors.email && <div className="error">
-                                            {errors.email}
-                                        </div>}
-                                    </div>
-                                    <button className="btn btn-primary" type="submit">Enviar</button>
+                        </div>
+                        <div className="mb-3">
+                            <label className="form-label">Apellido</label>
+                            <input
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
+                                type="text"
+                                className="form-control"
+                                id="apellido"
+                                name="apellidp"
+                                placeholder="Ingrese su Apellido"
+                                value={formik.values.apellidp}
+                            />
+                            {formik.touched.apellidp && formik.errors.apellidp ?(
+                                     <div className="errors"> {formik.errors.apellidp} </div>
+                                 ):null} 
+                        </div>
+                        <div className="mb-3">
+                            <label className="form-label">Dirección</label>
+                            <input
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
+                                type="text"
+                                className="form-control"
+                                id="direccion"
+                                name="direccion"
+                                placeholder="Ingrese su dirección"
+                                value={formik.values.direccion}
+                            />
+                           {formik.touched.direccion && formik.errors.direccion ?(
+                                     <div className="errors"> {formik.errors.direccion} </div>
+                                 ):null} 
+                        </div>
+                        <div className="mb-3">
+                            <label className="form-label">Teléfono</label>
+                            <input
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
+                                type="text"
+                                className="form-control"
+                                id="telefono"
+                                name="telefono"
+                                placeholder="Ingrese su telefono"
+                                value={formik.values.telefono}
+                            />
+                       {formik.touched.telefono && formik.errors.telefono ?(
+                                     <div className="errors"> {formik.errors.telefono} </div>
+                                 ):null} 
+                        </div>
+                        <div className="mb-3">
+                            <label className="form-label">E-mail</label>
+                            <input
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
+                                type="text"
+                                className="form-control"
+                                id="email"
+                                name="email"
+                                placeholder="Ingrese su email"
+                                value={formik.values.email}
+                            />
+                              {formik.touched.email && formik.errors.email ?(
+                                     <div className="errors"> {formik.errors.email} </div>
+                                 ):null} 
+                        </div>
+                        <input type="submit" className="btn btn-primary" value="Enviar"/>
 
 
-                                </Form>
-
-
-                            )
-                        }
-
-                    </Formik>
+                    </form>
                 </div>
                 <div className="col-md-6">
                     <table className="table">
